d81d917543743fc07beb05f2d7945a5b0e8daff1
Cody Sheridan <sheridan.codyj@gmail.com>
2021-07-13 16:21:13 -0400

    Passing tokens during registration.

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -1,6 +1,8 @@
 import React, { useReducer } from 'react';
+import axios from 'axios';
 import AuthContext from './authContext';
 import authReducer from './authReducer';
+import setAuthToken from '../../utils/setAuthToken';
 
 import {
 	LOGOUT,
@@ -25,14 +27,56 @@ const AuthState = props => {
 	const [state, dispatch] = useReducer(authReducer, initialState);
 
 	// Load user.
+	const loadUser = async () => {
+		setAuthToken(localStorage.token);
+
+		try {
+			const res = await axios.get('/api/auth');
+
+			dispatch({
+				type: USER_LOADED,
+				payload: res.data,
+			});
+		} catch (err) {
+			dispatch({
+				type: AUTH_ERROR,
+			});
+		}
+	};
 
 	// Register user.
+	const register = async formData => {
+		const config = {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		};
+
+		try {
+			const res = await axios.post('/api/users', formData, config);
+
+			dispatch({
+				type: REGISTER_SUCCESS,
+				payload: res.data,
+			});
+
+			loadUser();
+		} catch (err) {
+			dispatch({
+				type: REGISTER_FAIL,
+				payload: err.response.data.msg,
+			});
+		}
+	};
 
 	// Login user and get token.
+	const login = () => console.log('login user');
 
 	// Logout user and destroy token.
+	const logout = () => console.log('Logout user');
 
 	// Clear errors.
+	const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
 
 	return (
 		<AuthContext.Provider
@@ -42,6 +86,11 @@ const AuthState = props => {
 				loading: state.loading,
 				user: state.user,
 				error: state.error,
+				register,
+				loadUser,
+				login,
+				logout,
+				clearErrors,
 			}}
 		>
 			{props.children}
---
+++ client/src/context/auth/authReducer.js
@@ -0,0 +1,50 @@
+import {
+	LOGOUT,
+	LOGIN_FAIL,
+	AUTH_ERROR,
+	USER_LOADED,
+	CLEAR_ERRORS,
+	LOGIN_SUCCESS,
+	REGISTER_FAIL,
+	REGISTER_SUCCESS,
+} from '../types';
+
+export default (state, action) => {
+	switch (action.type) {
+		case USER_LOADED:
+			return {
+				...state,
+				isAuthenticated: true,
+				loading: false,
+				user: action.payload,
+			};
+		case REGISTER_SUCCESS:
+			// Save JWT token to local storage.
+			localStorage.setItem('token', action.payload.token);
+			return {
+				...state,
+				...action.payload,
+				isAuthenticated: true,
+				loading: false,
+			};
+		case REGISTER_FAIL:
+		case AUTH_ERROR:
+			// Remove token from local storage.
+			localStorage.removeItem('token');
+			return {
+				...state,
+				token: null,
+				isAuthenticated: false,
+				loading: false,
+				user: null,
+				error: action.payload,
+			};
+		case CLEAR_ERRORS:
+			return {
+				...state,
+				error: null,
+			};
+		default:
+			return state;
+	}
+};
